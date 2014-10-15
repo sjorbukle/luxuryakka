bd7b4a085cc607943b513f867ee0d6052864b1e4
Marko Kranjac <mkranjac@gmail.com>
2014-10-15 16:10:11 +0200

    frontend angular seed

--- frontend/admin/app/app/logistika/logistika.controllers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Laplacian d.o.o. on 5/14/14.
- */
-
-angular.module('logistika.core')
-.controller('GeneralSettingsCtrl', ['$scope', function ($scope) {
-    $scope.token = 'navigation@app tOkEN';
-    console.log($scope.token);
-}]);
