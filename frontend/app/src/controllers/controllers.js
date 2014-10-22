240468a543de7ebb868ec5ba5f3375a3e01f81b8
Josip Medic <josipmedic@gmail.com>
2014-10-22 18:32:01 +0200

    changes in backend and fronted

--- frontend/app/src/controllers/controllers.js
+++ frontend/app/src/controllers/controllers.js
@@ -1,8 +1,8 @@
 define(['angular'], function(angular) {
     angular.module('app.controllers', [])
     .controller('MainCtrl', ['$scope', function($scope) {
-        $scope.pageTitle = 'JMedic';
-        }])
+        $scope.pageTitle = 'Luxury Akka';
+    }])
     .controller('Widget', ['$scope', function($scope) {
     }])
 });
---
+++ frontend/app/src/services/constants.js
@@ -0,0 +1,8 @@
+angular.module('com.laplacian.luxuryakka.constants', [])
+
+    .constant('REFRESH_TOKEN_VALID_TIME', 300000)
+
+    .constant('TOKEN', 'luxury-akka-token')
+
+    .constant('CLOUD', 'localhost:9000')
+;
