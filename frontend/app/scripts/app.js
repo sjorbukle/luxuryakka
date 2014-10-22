06d6228f2c76bb784c34fae642ad85dfae503ada
Marko Kranjac <mkranjac@gmail.com>
2014-10-22 11:38:12 +0200

    theme

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -1,15 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name adminApp
- * @description
- * # adminApp
- *
- * Main module of the application.
- */
-angular
-  .module('adminApp', [
+angular.module('adminApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
