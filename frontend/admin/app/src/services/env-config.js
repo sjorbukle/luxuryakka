b412f181cccd520519fe21ca0d9d20d1cce811be
Josip Medic <josipmedic@gmail.com>
2014-10-30 14:52:12 +0100

    changes

--- frontend/admin/app/src/services/env-config.js
+++ frontend/admin/app/src/services/env-config.js
@@ -1,7 +1,7 @@
 define(["angular"], function(angular) {
  angular.module('envconfig', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://luxury-akka-josip.herokuapp.com'})
+.constant('ENV', {name:'development',apiEndpoint:'http://0.0.0.0:9000'})
 
 .constant('TOKEN', 'luxury-akka-token')
 
