12887a20e16aefb3244517f6b13010be3d1453ee
Josip Medic <josipmedic@gmail.com>
2014-11-10 12:12:50 +0100

    schange who knows what

--- frontend/admin/app/src/config.js
+++ frontend/admin/app/src/config.js
@@ -5,14 +5,18 @@ requirejs.config({
         'angular-mocks': '../bower_components/angular-mocks/angular-mocks',
         'angular-resource': '../bower_components/angular-resource/angular-resource',
         'angular-route': '../bower_components/angular-route/angular-route',
-        'angular-sanitize': ['//ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular-sanitize'],
+        'angular-sanitize': [
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular-sanitize'
+        ],
         jquery: '../bower_components/jquery/dist/jquery',
         jwt_services: 'services/jwt_services',
         services: 'services/services',
         envconfig: 'services/env-config',
         controllers: 'controllers/controllers',
         requirejs: '../bower_components/requirejs/require',
-        textAngular: ['//cdnjs.cloudflare.com/ajax/libs/textAngular/1.2.2/textAngular.min'],
+        textAngular: [
+            '//cdnjs.cloudflare.com/ajax/libs/textAngular/1.2.2/textAngular.min'
+        ],
         'bootstrap-sass': '../bower_components/bootstrap-sass/dist/js/bootstrap',
         underscore: '../bower_components/underscore/underscore'
     },
@@ -40,7 +44,10 @@ requirejs.config({
                 'jquery'
             ]
         },
-        textAngular: ['angular','angular-sanitize']
+        textAngular: [
+            'angular',
+            'angular-sanitize'
+        ]
     },
     packages: [
 
