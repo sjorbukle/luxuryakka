eca96aa83f86f24d1d6cbc86b789765ea9aa502d
Josip Medic <josipmedic@gmail.com>
2014-11-28 12:04:47 +0100

    added changes in versioning with specifinyng version numbers

--- frontend/admin/app/src/config.js
+++ frontend/admin/app/src/config.js
@@ -5,9 +5,6 @@ requirejs.config({
         'angular-mocks': '../bower_components/angular-mocks/angular-mocks',
         'angular-resource': '../bower_components/angular-resource/angular-resource',
         'angular-route': '../bower_components/angular-route/angular-route',
-        'angular-sanitize': [
-            '//ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular-sanitize'
-        ],
         jquery: '../bower_components/jquery/dist/jquery',
         envconfig: 'env-config',
         user: 'user/module',
@@ -29,7 +26,8 @@ requirejs.config({
         ],
         'bootstrap-sass': '../bower_components/bootstrap-sass/dist/js/bootstrap',
         underscore: '../bower_components/underscore/underscore',
-        'angular-ui-select': '../bower_components/angular-ui-select/dist/select'
+        'angular-ui-select': '../bower_components/angular-ui-select/dist/select',
+        'angular-sanitize': '../bower_components/angular-sanitize/angular-sanitize'
     },
     shim: {
         angular: {
@@ -44,10 +42,10 @@ requirejs.config({
         'angular-resource': [
             'angular'
         ],
-        'angular-route': [
+        'angular-sanitize': [
             'angular'
         ],
-        'angular-sanitize': [
+        'angular-route': [
             'angular'
         ],
         'angular-ui-select': {
